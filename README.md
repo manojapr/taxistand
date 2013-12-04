210f8aaab30b0aab2745ac775bbc475f7f638a8f
Manoj Parida <parida.manoja@gmail.com>
2013-12-04 19:16:58 +0530

    upto Micropost pulled from github

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+This is a application to make the taxi service in India as free trade. It will help customers find taxi available in their local area as easy as possible and most importantly with very low cost.
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,24 @@
+class StaticPagesController < ApplicationController
+  def home
+    @micropost = current_user.microposts.build if signed_in?
+  end
+  
+
+  def help
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+  
+  
+end
